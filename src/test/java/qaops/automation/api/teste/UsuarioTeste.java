ea7683c3060d481d462e2492c0c1444183b5f18d
Aline595 <aline.as385@gmail.com>
2021-08-12 17:18:51 -0300

    Criando classe pai baseTeste

--- src/test/java/qaops/automation/api/teste/UsuarioTeste.java
+++ src/test/java/qaops/automation/api/teste/UsuarioTeste.java
@@ -3,25 +3,16 @@
  */
 package qaops.automation.api.teste;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import qaops.automation.api.dominio.Usuario;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class UsuarioTeste {
-
-    @BeforeClass
-    public static void setup(){
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Define para log seja mais verboso e facil de entender.
-        baseURI = "https://reqres.in"; // Define url base de todos os testes
-        basePath = "/api";  // define caminho base de todos os testes
-    }
+public class UsuarioTeste extends BaseTest{
 
     // Primeiro teste: Listagem de usuário
     @Test
